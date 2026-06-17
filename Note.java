d4a6ae4e0e3731b82c25d72a2562d1353d346d7c
sristysandeep <deepusristy@gmail.com>
2026-06-17 13:15:22 +0100

    Add files via upload

---
+++ Note.java
@@ -0,0 +1,17 @@
+
+package com.example.demo.model;
+
+public class Note {
+    private int id;
+    private String title;
+    private String content;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
